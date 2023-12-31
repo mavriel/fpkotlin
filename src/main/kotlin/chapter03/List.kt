6c19ca0d98336c368837e778fe6e276d46effcae
mavriel <mavriel@gmail.com>
2023-12-31 21:41:49 +0900

    chapter03 - 95

--- src/main/kotlin/chapter03/List.kt
+++ src/main/kotlin/chapter03/List.kt
@@ -12,11 +12,55 @@ sealed class List<out T> {
 }
 
 fun <A> tail(xs: List<A>): List<A> = when (xs) {
-    is Nil -> Nil
+    is Nil -> xs
     is Cons -> xs.tail
 }
 
 fun <A> setHead(xs: List<A>, x: A): List<A> = Cons(x, tail(xs))
+
+fun <A> drop(l: List<A>, n: Int): List<A> = when (l) {
+    is Nil -> l
+    is Cons -> if (n == 0) l else drop(l.tail, n - 1)
+}
+
+fun <A> dropWhile(l: List<A>, f: (A) -> Boolean): List<A> = when (l) {
+    is Nil -> l
+    is Cons -> if (f(l.head)) dropWhile(tail(l), f) else l
+}
+
+fun <A> append(a1: List<A>, a2: List<A>): List<A> = when (a1) {
+    is Nil -> a2
+    is Cons -> Cons(a1.head, append(a1.tail, a2))
+}
+
+// 앞에서부터 끝까지 모두 계산해야 되서 n만큼 시간 걸림
+fun <A> init(l: List<A>): List<A> = when (l) {
+    is Nil -> l
+    is Cons -> when (l.tail) {
+        is Nil -> Nil
+        is Cons -> Cons(l.head, init(l.tail))
+    }
+}
+
+fun <A, B> foldRight(xs: List<A>, z: B, f: (A, B) -> B): B = when (xs) {
+    is Nil -> z
+    is Cons -> f(xs.head, foldRight(xs.tail, z, f))
+}
+
+tailrec fun <A, B> foldLeft(xs: List<A>, z: B, f: (B, A) -> B): B = when (xs) {
+    is Nil -> z
+    is Cons -> foldLeft(xs.tail, f(z, xs.head), f)
+}
+
+fun sum2(xs: List<Int>) = foldLeft(xs, 0) { a, b -> a + b }
+
+fun product2(xs: List<Int>) = foldLeft(xs, 1) { a, b -> a * b }
+
+fun <A> length(xs: List<A>) = foldRight(xs, 0) { _, acc -> acc + 1 }
+
+fun <A> empty(): List<A> = Nil
+
+fun <A> revert(xs: List<A>) = foldLeft(xs, empty<A>()) { acc, a -> Cons(a, acc) }
 
 data object Nil : List<Nothing>() {
     override fun get(n: Int): Nothing? = null
@@ -28,5 +72,17 @@ data class Cons<out A>(val head: A, val tail: List<A>) : List<A>() {
             return head
         }
         return tail.get(n - 1)
+    }
+}
+
+// 3.13
+fun <A> append2(a1: List<A>, a2: List<A>): List<A> = foldLeft(a1, a2) { acc, i -> Cons(i, acc) }
+fun <A> append3(a1: List<A>, a2: List<A>): List<A> = foldRight(a1, a2) { i, acc -> Cons(i, acc) }
+
+// 3.14
+fun <A> flatten(xxs: List<List<A>>): List<A> = foldLeft(xxs, empty()) { xs1, xs2 ->
+    when (xs2) {
+        is Nil -> xs1
+        is Cons -> append(xs1, xs2)
     }
 }
--- src/test/kotlin/fp/kotlin/chapter03/ListTest.kt
+++ src/test/kotlin/fp/kotlin/chapter03/ListTest.kt
@@ -4,11 +4,10 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ListTest {
+    private val list = List.of(1, 20, 300, 4000)
 
     @Test
     fun of() {
-        val list = List.of(1, 20, 300, 4000)
-
         assertEquals(1, list.get(0))
         assertEquals(20, list.get(1))
         assertEquals(300, list.get(2))
@@ -17,8 +16,6 @@ class ListTest {
 
     @Test
     fun tail() {
-        val list = List.of(1, 20, 300, 4000)
-
         assertEquals(20, tail(list).get(0))
         assertEquals(300, tail(tail(list)).get(0))
         assertEquals(4000, tail(tail(tail(list))).get(0))
@@ -26,8 +23,73 @@ class ListTest {
 
     @Test
     fun setHead() {
-        val list = List.of(1, 20, 300, 4000)
-
         assertEquals(100, setHead(list, 100).get(0))
+    }
+
+    @Test
+    fun drop() {
+        assertEquals(20, drop(list, 1).get(0))
+        assertEquals(300, drop(list, 2).get(0))
+        assertEquals(4000, drop(list, 3).get(0))
+    }
+
+    @Test
+    fun dropWhile() {
+        assertEquals(4000, dropWhile(list) { it < 4000 }.get(0))
+    }
+
+    @Test
+    fun init() {
+        assertEquals(null, init(list).get(3))
+        assertEquals(300, init(list).get(2))
+        assertEquals(20, init(list).get(1))
+        assertEquals(1, init(list).get(0))
+        assertEquals(4000, list.get(3))
+    }
+
+    @Test
+    fun length() {
+        assertEquals(4, length(list))
+    }
+
+    @Test
+    fun sum2_product2() {
+        assertEquals(4321, sum2(list))
+        assertEquals(24000000, product2(list))
+    }
+
+    @Test
+    fun revert() {
+        val l = revert(list)
+        assertEquals(1, l.get(3))
+        assertEquals(20, l.get(2))
+        assertEquals(300, l.get(1))
+        assertEquals(4000, l.get(0))
+    }
+
+    @Test
+    fun append2() {
+        val newList = append2(list, Cons(5000, Cons(6000, Nil)))
+        assertEquals(5000, newList.get(4))
+        assertEquals(6000, newList.get(5))
+    }
+
+    @Test
+    fun append3() {
+        val newList = append3(list, Cons(5000, Cons(6000, Nil)))
+        assertEquals(5000, newList.get(4))
+        assertEquals(6000, newList.get(5))
+    }
+
+    @Test
+    fun flatten() {
+        val l = flatten(Cons(List.of(9, 8, 7), Cons(empty<Int>(), Cons(List.of(6, 5, 4), Nil))))
+
+        assertEquals(9, l.get(0))
+        assertEquals(8, l.get(1))
+        assertEquals(7, l.get(2))
+        assertEquals(6, l.get(3))
+        assertEquals(5, l.get(4))
+        assertEquals(4, l.get(5))
     }
 }
