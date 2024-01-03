7158f6a32cf6f906b388a1b647b74c8181ae13b4
mavriel <mavriel@gmail.com>
2024-01-03 20:05:55 +0900

    4.2

--- src/main/kotlin/chapter04/Option.kt
+++ src/main/kotlin/chapter04/Option.kt
@@ -1,5 +1,7 @@
 package fp.kotlin.chapter04
 
+import kotlin.math.pow
+
 sealed class Option<out A>
 
 data class Some<out A>(val get: A) : Option<A>()
@@ -22,3 +24,8 @@ fun <A, B> Option<A>.flatMap(f: (A) -> Option<B>): Option<B> = this.map(f).getOr
 fun <A> Option<A>.orElse(ob: () -> Option<A>): Option<A> = this.map { Some(it) }.getOrElse { ob() }
 
 fun <A> Option<A>.filter(f: (A) -> Boolean): Option<A> = this.flatMap { if (f(it)) Some(it) else None }
+
+fun mean(xs: List<Double>): Option<Double> = if (xs.isEmpty()) None else Some(xs.sum() / xs.size)
+
+fun variance(xs: List<Double>): Option<Double> = mean(xs).map { m -> xs.map { x -> (x - m).pow(2) }.sum() / xs.size }
+fun variance2(xs: List<Double>): Option<Double> = mean(xs).flatMap { m -> mean(xs.map { x -> (x - m).pow(2) }) }
